adbd5eb9a3466e6146dcaeca88239babd3211055
wind <1957334229@qq.com>
2025-12-01 16:47:23 +0800

    工程项目生成（浏览和部署）
    1. 开发 Vue 项目构造器，安装依赖并构建项目
    2. JSON 流处理完成时，执行 Vue 项目构建
    3. 修改 deployApp 方法，支持 Vue 项目的部署

--- src/main/java/com/qian/aicodemother/service/impl/AppServiceImpl.java
+++ src/main/java/com/qian/aicodemother/service/impl/AppServiceImpl.java
@@ -9,6 +9,7 @@ import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.spring.service.impl.ServiceImpl;
 import com.qian.aicodemother.constant.AppConstant;
 import com.qian.aicodemother.core.AiCodeGeneratorFacade;
+import com.qian.aicodemother.core.builder.VueProjectBuilder;
 import com.qian.aicodemother.core.handler.StreamHandlerExecutor;
 import com.qian.aicodemother.exception.BusinessException;
 import com.qian.aicodemother.exception.ErrorCode;
@@ -56,6 +57,9 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App> implements AppSe
 
     @Resource
     StreamHandlerExecutor streamHandlerExecutor;
+
+    @Resource
+    VueProjectBuilder vueProjectBuilder;
 
 
     @Override
@@ -113,21 +117,33 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App> implements AppSe
         if (!sourceDir.exists() || !sourceDir.isDirectory()) {
             throw new BusinessException(ErrorCode.SYSTEM_ERROR, "应用代码路径不存在，请先生成应用");
         }
-        //7.复制文件到部署目录
+        //7. vue 项目特殊处理：执行构建
+        CodeGenTypeEnum codeGenTypeEnum = CodeGenTypeEnum.getEnumByValue(codeGenType);
+        if (codeGenTypeEnum == CodeGenTypeEnum.VUE_PROJECT) {
+            // Vue 项目需要构建
+            boolean buildSuccess = vueProjectBuilder.buildProject(sourceDirPath);
+            ThrowUtils.throwIf(!buildSuccess, ErrorCode.SYSTEM_ERROR, "Vue 项目构建失败，请重试");
+            // 检查 dist 目录是否存在
+            File distDir = new File(sourceDirPath, "dist");
+            ThrowUtils.throwIf(!distDir.exists() , ErrorCode.SYSTEM_ERROR, "Vue 项目构建完成，但未生成 dist 目录");
+            // 构建完成后，需要将构建后的文件复制到部署目录
+            sourceDir = distDir;
+        }
+        //8.复制文件到部署目录
         String deployDirPath = AppConstant.CODE_DEPLOY_ROOT_DIR + File.separator + deployKey;
         try {
             FileUtil.copyContent(sourceDir, new File(deployDirPath), true);
         } catch (Exception e) {
             throw new BusinessException(ErrorCode.SYSTEM_ERROR, "应用部署失败" + e.getMessage());
         }
-        //8. 更新数据库
+        //9. 更新数据库
         App updateApp = new App();
         updateApp.setId(appId);
         updateApp.setDeployKey(deployKey);
         updateApp.setDeployedTime(LocalDateTime.now());
         boolean updateResult = this.updateById(updateApp);
         ThrowUtils.throwIf(!updateResult, ErrorCode.OPERATION_ERROR, "更行应用部署信息失败");
-        //9. 返回部署可访问的 URL 地址
+        //10. 返回部署可访问的 URL 地址
         return String.format("%s/%s", AppConstant.CODE_DEPLOY_HOST, deployKey);
     }
 
