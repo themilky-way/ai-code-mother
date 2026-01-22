762dd9c1e84791089da27cd03bd0e152c42b4a11
wind <1957334229@qq.com>
2026-01-22 13:13:02 +0800

    AI 工作流（LangGraph4j 高级特性 - 图片并发收集）
    1. 基于 LangGraph4j Parallel Branch 实现并发

--- src/main/java/com/qian/aicodemother/langgraph4j/state/WorkflowContext.java
+++ src/main/java/com/qian/aicodemother/langgraph4j/state/WorkflowContext.java
@@ -1,5 +1,6 @@
 package com.qian.aicodemother.langgraph4j.state;
 
+import com.qian.aicodemother.langgraph4j.model.ImageCollectionPlan;
 import com.qian.aicodemother.langgraph4j.model.ImageResource;
 import com.qian.aicodemother.langgraph4j.model.QualityResult;
 import com.qian.aicodemother.model.enums.CodeGenTypeEnum;
@@ -77,6 +78,21 @@ public class WorkflowContext implements Serializable {
      * 错误信息
      */
     private String errorMessage;
+
+    /**
+     * 图片收集计划
+     */
+    private ImageCollectionPlan imageCollectionPlan;
+
+
+    /**
+     * 并发图片收集的中间结果字段
+     */
+    private List<ImageResource> contentImages;
+    private List<ImageResource> illustrations;
+    private List<ImageResource> diagrams;
+    private List<ImageResource> logos;
+
 
     @Serial
     private static final long serialVersionUID = 1L;
