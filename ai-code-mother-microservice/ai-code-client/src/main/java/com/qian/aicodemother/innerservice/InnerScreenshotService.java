3940e79cc89ca0d0e818827fa63d0fb994fd873b
wind <1957334229@qq.com>
2026-06-07 15:51:53 +0800

    微服务改造 - 服务接口模块（中间态，影响了单体项目代码，不要单独使用当前提交）

---
+++ ai-code-mother-microservice/ai-code-client/src/main/java/com/qian/aicodemother/innerservice/InnerScreenshotService.java
@@ -0,0 +1,10 @@
+package com.qian.aicodemother.innerservice;
+
+/**
+ * 内部截图服务
+ */
+public interface InnerScreenshotService {
+
+    String generateAndUploadScreenshot(String webUrl);
+
+}
---
+++ ai-code-mother-microservice/ai-code-client/src/main/java/com/qian/aicodemother/innerservice/InnerUserService.java
@@ -0,0 +1,35 @@
+package com.qian.aicodemother.innerservice;
+
+import com.qian.aicodemother.exception.BusinessException;
+import com.qian.aicodemother.exception.ErrorCode;
+import com.qian.aicodemother.model.entity.User;
+import com.qian.aicodemother.model.vo.UserVO;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+import static com.qian.aicodemother.constant.UserConstant.USER_LOGIN_STATE;
+
+/**
+ * 内部使用的用户服务
+ */
+public interface InnerUserService {
+
+    List<User> listByIds(Collection<? extends Serializable> ids);
+
+    User getById(Serializable id);
+
+    UserVO getUserVO(User user);
+
+    // 静态方法，避免跨服务调用
+    static User getLoginUser(HttpServletRequest request) {
+        Object userObj = request.getSession().getAttribute(USER_LOGIN_STATE);
+        User currentUser = (User) userObj;
+        if (currentUser == null || currentUser.getId() == null) {
+            throw new BusinessException(ErrorCode.NOT_LOGIN_ERROR);
+        }
+        return currentUser;
+    }
+}
